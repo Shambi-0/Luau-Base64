9de24b42d091ef40512219bf819afeb1b48d4184
Shambi <87273502+Shambi-0@users.noreply.github.com>
2022-06-26 08:02:07 -0500

    Update Index.lua
    - Improved Syntax.
    - Bug fixes.

--- Index.lua
+++ Index.lua
@@ -1,6 +1,6 @@
 --[[::
 
-Copyright (C) 2021, Luc Rodriguez (Aliases : Shambi, StyledDev).
+Copyright (C) 2022, Luc Rodriguez (Aliases : Shambi, StyledDev).
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,70 +20,92 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
+(Original Repository can be found here : https://github.com/Shambi-0/Luau-Base64)
+
 --::]]
 
-type Dictionary<Type> = {[string] : Type};
-type Array<Type> = {[number] : Type};
+--------------------------
+--// Type Definitions //--
+--------------------------
 
-local Base64 : Dictionary<any?> = {
-	["Dictionary"] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+type Array<Type> = {[number] : Type};
+type Dictionary<Type> = {[string] : Type};
+
+-----------------------
+--// Initalization //--
+-----------------------
+
+local Module : Dictionary<string | (string) -> string> = {
+	["Library"] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 };
 
-function Base64:Encode(Data : string) : string
-	return (string.gsub(string.gsub(Data, '.', function(Character) : string
-		local Result : string, Byte : number = '', string.byte(Character)
+-----------------
+--// Methods //--
+-----------------
+
+function Module:Encode(Data : string) : string
+	local InputType : string = type(Data);
+	assert(InputType == "string", string.format("Expected type \"string\" for argument #1 of \"Base64:Decode()\". Recieved type \"%s\".", InputType));
+
+	return (string.gsub(string.gsub(Data, ".", function(Chunk : string) : string?
+		local Result : string, Byte : number = "", string.byte(Chunk);
 
 		for Index : number = 8, 1, -1 do
-			Result ..= if (bit32.band(Byte, (2 ^ Index) - 1) - bit32.band(Byte, (2 ^ (Index - 1)) - 1) > 0) then '1' else '0';
+			Result ..= if (Byte % 2 ^ Index - Byte % 2 ^ (Index - 1) > 0) then '1' else '0';
 		end;
 
-		return (Result);
-		
-	end) .. '0000', "%d%d%d?%d?%d?%d?", function(Character : string) : string
-		if (#Character < 6) then
-			return ('');
+		return Result;
+
+	end) .. "0000", "%d%d%d?%d?%d?%d?", function(Chunk : string) : string?
+		if (string.len(Chunk) < 6) then
+			return "";
 		end;
 
-		local Counter : number = 0;
+		local Count : number = 0;
 
 		for Index : number = 1, 6 do
-			Counter += if (string.sub(Character, Index, Index) == '1') then (2 ^ (6 - Index)) else 0;
+			Count += if (string.sub(Chunk, Index, Index) == "1") then 2 ^ (6 - Index) else 0;
 		end;
 
-		Counter += 1;
+		return string.sub(self.Library, Count + 1, Count + 1);
 
-		return (string.sub(self.Dictionary, Counter, Counter));
-		
-	end) .. ({ '', '==', '=' })[#Data % 3 + 1]);
+	end) .. ({ "", "==", "=" })[string.len(Data) % 3 + 1]);
 end;
 
-function Base64:Decode(Data : string) : string
-	return (string.gsub(string.gsub(string.gsub(Data, '[^' .. self.Dictionary .. '=]', ''), '.', function(Character : string) : string
-		if (Character == '=') then
-			return ('');
+function Module:Decode(Data : string) : string
+	local InputType : string = type(Data);
+	assert(InputType == "string", string.format("Expected type \"string\" for argument #1 of \"Base64:Decode()\". Recieved type \"%s\".", InputType));
+
+	return (string.gsub(string.gsub(string.gsub(Data, "[^" .. self.Library .. "=]", ""), '.', function(Chunk : string) : string?
+		if (Chunk == "=") then
+			return "";
 		end;
 
-		local Result : string, Format : number = '', string.find(self.Dictionary, Character) - 1;
+		local Result : string, Found : number = "", string.find(self.Library, Chunk) - 1;
 
 		for Index : number = 6, 1, -1 do
-			Result ..= if (bit32.band(Format, (2 ^ Index) - 1) - bit32.band(Format, (2 ^ (Index - 1)) - 1) > 0) then '1' else '0';
+			Result ..= if (Found % 2 ^ Index - Found % 2 ^ (Index - 1) > 0) then "1" else "0";
 		end;
 
-		return (Result);
-		
-	end), "%d%d%d?%d?%d?%d?%d?%d?", function(Pair : string) : string
-		if (#Pair ~= 8) then
-			return ('');
+		return Result;
+
+	end), "%d%d%d?%d?%d?%d?%d?%d?", function(Chunk : string) : string?
+		if (string.len(Chunk) ~= 8) then
+			return "";
 		end;
 
-		local Counter : number = 0;
+		local Count : number = 0;
 
 		for Index : number = 1, 8 do
-			Counter += if (string.sub(Pair, Index, Index) == '1') then (2 ^ (7 - Index)) else 0;
+			Count += if (string.sub(Chunk, Index, Index) == "1") then 2 ^ (8 - Index) else 0;
 		end;
 
-		return (string.char(Counter));
+		return string.char(Count);
 	end));
 end;
 
-return (Base64);
+----------------------
+--// Finalization //--
+----------------------
+
+return (Module);
